ed1b7df87ecec64bdc19c9b46a9311055dd7d016
meder <med@imap.cc>
2019-04-02 21:58:24 +0200

    Multi Threading

---
+++ multi-thread.py
@@ -0,0 +1,29 @@
+"""
+Simple demonstration of multi-threading
+"""
+
+import logging
+import threading
+import time
+
+def thread_function(name):
+    """ A simple function that just logs and sleeps in between """
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+if __name__ == "__main__":
+    fmt = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=fmt, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    threads = list()
+    for i in range(3):
+        logging.info("Starting " + str(i))
+        x = threading.Thread(target=thread_function, args=(i,))
+        x.start()
+        threads.append(x)
+
+    for index, thread in enumerate(threads):
+        thread.join()
+        logging.info("Thread %s done", thread)
